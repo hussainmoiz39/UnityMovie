27c264c3397f6b16e64850575c3678e93e34105a
hussainmoiz39 <hussainmoiz.39@gmail.com>
2017-03-28 21:43:37 +0530

    First Commit

---
+++ alien space ship/Assets/space.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class space : MonoBehaviour {
+    public int a = 0;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        a++;
+        if (a > 0)
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * 1000);
+        }
+    }
+}
